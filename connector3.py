de450143afbeed8dc959cad2ce8c83dca02c26c4
Pinku sonkar <pinkusonkar63@gmail.com>
2026-01-30 19:34:44 +0530

    Add files via upload

---
+++ connector3.py
@@ -0,0 +1,40 @@
+import time as t
+import mysql.connector
+
+
+def bill():
+    con = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="pinku@123", #enter your password
+    database="shop" #enter your database name
+    )
+    cur = con.cursor()
+    Order_id=int(input("enter the order id: "))
+    Product_Name=input("enter the product name: ")
+    P_Price=float(input("enter the P_Price: "))
+    Quantity=int(input("enter the Quantity: "))
+    Amount=P_Price*Quantity
+    sql = "INSERT INTO Products (Order_id,Product_name,P_Price,Quantity,Amount) VALUES (%s,%s,%s,%s,%s)"
+    values = (Order_id,Product_Name,P_Price,Quantity,Amount)
+    cur.execute(sql,values)
+    con.commit()
+    print("✅ Data successfully inserted into Products table!")
+
+    amount=P_Price*Quantity
+    name=str(Order_id)
+    time=t.ctime()
+    f = open(name + ".txt", "w")
+    data=f.write(f'''
+    Time: {time}
+    --------------------
+    order id: {Order_id}
+    product name: {Product_Name}
+    Quantity: {Quantity}
+    ---------------------
+    final amount: {amount}''')
+    f.close()
+    cur.close()
+    con.close()
+    
+bill()
